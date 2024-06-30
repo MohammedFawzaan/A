9b10e82bc5ffcc8cc8bc78c5027b4b57e8698d5a
Mohammed Fawzaan <fawzaanmd31@gmail.com>
2024-06-30 21:42:23 +0530

    Basic Setup

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-let a = 10;
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    console.log("Connected");
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(port, () => {
+    console.log(`App at ${port}`);
+});
